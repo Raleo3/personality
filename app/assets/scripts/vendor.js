26b2213b1d171ddb5789521df762ff93e1105e6e
Richard Leo <Richard.Leo@morningstar.com>
2017-05-29 14:26:37 -0400

    more tooling

---
+++ app/assets/scripts/vendor.js
@@ -0,0 +1,3 @@
+// Import Vendor Packages here
+import 'lazysizes';
+// import jQuery here & give it an alias like you have in EAMS-UI...plug in for that
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require("gulp"),
 	watch = require("gulp-watch"),
 	postcss = require("gulp-postcss"),
+	usemin = require("gulp-usemin"),
+	rev = require('gulp-rev'),
+	uglify = require('gulp-uglify'),
+	cssnano = require('gulp-cssnano'),
 	autoprefixer = require("autoprefixer"),
 	cssvars = require("postcss-simple-vars"),
 	mixins = require("postcss-mixins"),
@@ -10,6 +14,19 @@ var gulp = require("gulp"),
 	browserSync = require('browser-sync').create();
 
 
+gulp.task("deleteDistFolder", function () {
+	return del("./dist");
+});
+
+gulp.task("previewDist", function () {
+	browserSync.init({
+		server: {
+			baseDir: "dist"
+		}
+	});
+});
+
+// Compiles PostCSS/SCSS modules into single CSS file
 gulp.task("styles", function () {
 	return gulp.src("./app/assets/styles/styles.css")
 		.pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
@@ -17,9 +34,10 @@ gulp.task("styles", function () {
 			console.log(errorInfo.toString());
 			this.emit("end");
 		})
-		.pipe(gulp.dest("./app/static/styles"));
+		.pipe(gulp.dest("./app/temp/styles"));
 });
 
+// Runs webpack Module loader to bundle fresh js files according to config
 gulp.task("scripts", function (callback) {
 	webpack(require('./webpack.config.js'), function (err, stats) {
 		if (err) {
@@ -54,11 +72,38 @@ gulp.task("watch", function () {
 	});
 });
 
+// Auto refreshes dev server on all CSS file Changes
 gulp.task("cssInject", ['styles'], function () {
-	return gulp.src("./app/static/styles/styles.css")
+	return gulp.src("./app/temp/styles/styles.css")
 		.pipe(browserSync.stream());
 });
 
+// Auto refreshes dev server on all JS file Changes
 gulp.task("jsReload", ['scripts'], function () {
 	browserSync.reload();
 });
+
+// Deletes Dist Folder, Revisions, and Compresses JS & CSS bundle files, sending new PROD-ready versions to Dist folder
+gulp.task("usemin", ["deleteDistFolder", "styles", "scripts"], function() {
+	return gulp.src("./app/index.html")
+		.pipe(usemin({
+			css: [function() {
+				return rev()
+			},
+			function () {
+				return cssnano()
+			}],
+			js: [function () {
+				return rev()
+			},
+			function () {
+				return uglify
+			}]
+		}))
+		.pipe(gulp.dest("./dist"));
+})
+
+
+gulp.task("build", ["deleteDistFolder", "usemin"]);
+
+
